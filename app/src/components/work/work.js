34a1e38358af5829b8c779b3e045abe71187450c
MylennaAlcantara <mylenna.a.a.corinthiana@gmail.com>
2022-04-07 22:21:00 -0300

    main

--- app/src/components/work/work.js
+++ app/src/components/work/work.js
@@ -1,9 +1,24 @@
 import styled from "styled-components";
 
 const Container = styled.div`
-    background-color: orange;
+    background-color: hsl(15, 100%, 70%);
     height: 30vh;
     width: 30vh;
     grid-area: work;
+    padding-right: 15px;
+    border-radius: 10px;
+    align-items: flex-end;
+    justify-content: end;
+`
+export const Img = styled.div`
+    display: flex;
+    justify-content: flex-end;
+`
+export const Info = styled.div`
+    height: 58%;
+    width: 100%;
+    background-color: hsl(235, 46%, 20%);
+    border-radius: 10px;
+    position: relative;
 `
 export default Container;
--- app/src/components/wrapper/wrapper.js
+++ app/src/components/wrapper/wrapper.js
@@ -12,8 +12,27 @@ const Container = styled.div`
     grid-template-areas:
     "profile work play study"
     "profile exercise social self-care";
-    row-gap: 2rem;
-    column-gap: 2rem;
+    row-gap: 1.5rem;
+    column-gap: 1.5rem;   
+    @media (max-width: 500px){
+        height: 50vh;
+        width: 90vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        display: grid;
+        grid-template-columns: 1fr;
+        grid-template-rows: repeat(7, 1fr);
+        grid-template-areas:
+        "profile"
+        "work"
+        "play"
+        "study"
+        "exercise"
+        "social"
+        "self-care";
+    }
 `
+    
 
 export default Container;
